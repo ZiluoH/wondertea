c92a3bff128e5d988e1fdb39d302c6a4072e369e
ZiluoH <ziluoh@gmail.com>
2017-11-29 22:40:52 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-*
+// smooth scroll navbar
+$(document).ready(function(){
+	// add scrollspy to <body>
+	$('body').scrollspy({target:".navbar", offset:50});
+
+	// add smooth scrolling on all links inside the navbar
+	$('#navbarResponsive a').on('click', function(e){
+		// make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+			// prevent default anchor click behavior
+			e.preventDefault();
+
+			// store hash
+			var hash = this.hash;
+
+			// use jQuery's animate() method to add smooth page scroll
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function(){
+
+				// add hash(#) to url when done scrolling(default click behavior)
+				window.location.hash = hash;
+			})
+		}
+		// end if
+	})
+})
+
+
+// $('#navbarResponsive a')
